6e82d9a5b2c9022b6636afa3f4fd0f095922112e
Umutbek <karimovumutbek@gmail.com>
2021-10-31 03:02:34 +0600

    Constant(error responses) added

---
+++ Frontend/AdminPanel/lefke-market-admin/src/constants/errors.js
@@ -0,0 +1,5 @@
+export const ERRORS = {
+  LOGIN_FAIL_ERROR: 'Something went wrong...',
+  NETWORK_OR_SYNTAX_ERROR: 'Something went wrong.. Check your internet connection or contact us: lefkemarket@mail.com.',
+  SOMETHING_WENT_WRONG: 'Something went wrong...'
+}
