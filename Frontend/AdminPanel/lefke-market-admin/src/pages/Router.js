376c356f5f8121faa5d614a0804296375ca2144a
Umutbek <karimovumutbek@gmail.com>
2021-11-02 00:08:42 +0600

    Page 404 added

--- Frontend/AdminPanel/lefke-market-admin/src/pages/Router.js
+++ Frontend/AdminPanel/lefke-market-admin/src/pages/Router.js
@@ -3,6 +3,7 @@ import {Redirect, Route, Switch} from "react-router-dom"
 import WithBgSpinner from "../components/spinners/WithBgSpinner"
 
 const LoginPage = lazy(() => import('./auth/LoginPage'))
+const Page404 = lazy(() => import('./Page404'))
 
 export default function Router(){
   return (
@@ -11,9 +12,11 @@ export default function Router(){
         <Route exact path="/auth/login">
           <LoginPage/>
         </Route>
-
         <Route exact path="/login">
           <Redirect to="/auth/login" />
+        </Route>
+        <Route>
+          <Page404/>
         </Route>
 
       </Switch>
