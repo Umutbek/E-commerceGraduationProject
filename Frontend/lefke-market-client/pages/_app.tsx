7a69c4f271d80c91337b71e7fd08e2877d63f364
Umutbek <karimovumutbek@gmail.com>
2021-11-16 04:14:36 +0600

    Header mobile type added

--- Frontend/lefke-market-client/pages/_app.tsx
+++ Frontend/lefke-market-client/pages/_app.tsx
@@ -2,9 +2,16 @@ import React, {useEffect} from "react"
 import '../styles/globals.css'
 import {makeStyles, ThemeProvider} from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import {setScreenType} from "../src/redux/states/settings/actions"
+
 import {Provider} from "react-redux"
 import {AppBar, Hidden, useMediaQuery} from "@material-ui/core"
+
+import CollapseOnScroll from "../src/components/onScroll/collapseOnScroll"
 import DesktopHeader from "../src/components/header/desktop/desktop-header"
+import MobileHeader from "../src/components/header/mobile/mobile-header"
+import {ScreenTypes} from "../src/constants"
+
 import {initStore} from "../src/redux/store"
 import Head from 'next/head'
 const store = initStore()
@@ -22,6 +29,36 @@ const useStyles = makeStyles({
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const classes = useStyles()
+
+  const isLaptop = useMediaQuery('(max-width: 1360px')
+  const isMobile = useMediaQuery('(max-width: 768px')
+
+  useEffect(() => {
+
+    let screenType
+
+    if (isMobile){
+      screenType = ScreenTypes.mobile
+    } else if (isLaptop){
+      screenType = ScreenTypes.laptop
+    } else {
+      screenType = ScreenTypes.desktop
+    }
+
+    store.dispatch(setScreenType(screenType))
+
+  }, [isLaptop, isMobile])
+
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      // @ts-ignore
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
 
   return <>
     <Head>
@@ -32,6 +69,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <CssBaseline />
       <Hidden only={["xs", "sm", "md"]}>
         <DesktopHeader/>
+      </Hidden>
+      <Hidden only={["lg", "xl"]}>
+
+        <MobileHeader/>
       </Hidden>
 
       <Component {...pageProps} />
